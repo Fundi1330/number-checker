4cc720979baf8f7601d5ecbeee5dbe86c2cd8b8a
Fundi1330 <88210377+Fundi1330@users.noreply.github.com>
2022-03-07 17:46:00 +0200

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,12 +1,14 @@
+import math
+
 print("The program has started")
 
 while True:
-    sym = int(input("Enter a number, and I'll try to tell you about it: "))
+    sym = float(input("Enter a number, and I'll try to tell you about it: "))
     if (sym < 0):
         print("You entered a number: " + str(sym) + " it is negative")
-    elif (sym == 3.14):
+    elif (sym == math.pi or sym == 3.14):
         print("You entered the number P")
     elif (sym == 0):
         print("You entered zero")
     else:
-        print("You entered a positive number")
+        print("You entered a number: " + str(sym) + " it is positive")
